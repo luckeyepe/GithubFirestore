03964a2535d10f558db275a01ea54ea57cd42e11
luckeyepe <Gorilaman2>
2018-11-15 11:01:09 +0800

    added functionalities to the Cancel button in Add Student Activity

--- app/src/main/java/com/example/mickey/githubfirestore/activites/AddStudentActivity.kt
+++ app/src/main/java/com/example/mickey/githubfirestore/activites/AddStudentActivity.kt
@@ -3,11 +3,23 @@ package com.example.mickey.githubfirestore.activites
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mickey.githubfirestore.R
+import kotlinx.android.synthetic.main.activity_add_student.*
 
 class AddStudentActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_student)
+
+        btn_addStudentCancel.setOnClickListener {
+            edt_addStudentIDNumber.text = null
+            edt_addStudentFirstName.text = null
+            edt_addStudentMiddleName.text = null
+            edt_addStudentLastName.text =  null
+            edt_addStudentCoarse.text = null
+            edt_addStudentYearLevel.text = null
+        }
+
+        
     }
 }
