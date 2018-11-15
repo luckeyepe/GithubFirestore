399cb13887be6b019194fbc1dc5523dafefdcdd8
luckeyepe <Gorilaman2>
2018-11-15 10:41:23 +0800

    Added Delete Student, Search Student, and Update Student Activities

---
+++ app/src/main/java/com/example/mickey/githubfirestore/activites/SearchStudentActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mickey.githubfirestore.activites
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mickey.githubfirestore.R
+
+class SearchStudentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_student)
+    }
+}
---
+++ app/src/main/java/com/example/mickey/githubfirestore/activites/UpdateStudent.kt
@@ -0,0 +1,13 @@
+package com.example.mickey.githubfirestore.activites
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mickey.githubfirestore.R
+
+class UpdateStudent : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_student)
+    }
+}
